91d5ec0612e6e219888da7609972e868337b2ccf
fenny <fenny@DESKTOP-OC5036T>
2021-10-10 22:19:49 +0300

    Add project files.

---
+++ Photo/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿
+namespace Photo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.BtnRenkSec = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.BtnResimSec = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.BtnYazdir = new System.Windows.Forms.Button();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.panel15 = new System.Windows.Forms.Panel();
+            this.TxtMetin = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TxtBoyut = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel10.SuspendLayout();
+            this.panel11.SuspendLayout();
+            this.panel12.SuspendLayout();
+            this.panel14.SuspendLayout();
+            this.panel15.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel15);
+            this.panel1.Controls.Add(this.panel14);
+            this.panel1.Controls.Add(this.panel13);
+            this.panel1.Controls.Add(this.panel12);
+            this.panel1.Controls.Add(this.panel11);
+            this.panel1.Controls.Add(this.panel10);
+            this.panel1.Controls.Add(this.panel9);
+            this.panel1.Controls.Add(this.panel8);
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(212, 465);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(212, 38);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(212, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(667, 465);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.BtnRenkSec);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 38);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(212, 37);
+            this.panel4.TabIndex = 1;
+            // 
+            // BtnRenkSec
+            // 
+            this.BtnRenkSec.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnRenkSec.Location = new System.Drawing.Point(0, 0);
+            this.BtnRenkSec.Name = "BtnRenkSec";
+            this.BtnRenkSec.Size = new System.Drawing.Size(212, 37);
+            this.BtnRenkSec.TabIndex = 0;
+            this.BtnRenkSec.Text = "Renk Seç";
+            this.BtnRenkSec.UseVisualStyleBackColor = true;
+            this.BtnRenkSec.Click += new System.EventHandler(this.BtnRenkSec_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 75);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(212, 25);
+            this.panel5.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.BtnResimSec);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel6.Location = new System.Drawing.Point(0, 100);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(212, 37);
+            this.panel6.TabIndex = 3;
+            // 
+            // BtnResimSec
+            // 
+            this.BtnResimSec.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnResimSec.Location = new System.Drawing.Point(0, 0);
+            this.BtnResimSec.Name = "BtnResimSec";
+            this.BtnResimSec.Size = new System.Drawing.Size(212, 37);
+            this.BtnResimSec.TabIndex = 0;
+            this.BtnResimSec.Text = "Resim Seç";
+            this.BtnResimSec.UseVisualStyleBackColor = true;
+            this.BtnResimSec.Click += new System.EventHandler(this.BtnResimSec_Click);
+            // 
+            // panel7
+            // 
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(0, 137);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(212, 31);
+            this.panel7.TabIndex = 4;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.BtnYazdir);
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel8.Location = new System.Drawing.Point(0, 168);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(212, 37);
+            this.panel8.TabIndex = 5;
+            // 
+            // BtnYazdir
+            // 
+            this.BtnYazdir.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnYazdir.Location = new System.Drawing.Point(0, 0);
+            this.BtnYazdir.Name = "BtnYazdir";
+            this.BtnYazdir.Size = new System.Drawing.Size(212, 37);
+            this.BtnYazdir.TabIndex = 0;
+            this.BtnYazdir.Text = "Yazdır";
+            this.BtnYazdir.UseVisualStyleBackColor = true;
+            this.BtnYazdir.Click += new System.EventHandler(this.BtnYazdir_Click);
+            // 
+            // panel9
+            // 
+            this.panel9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel9.Location = new System.Drawing.Point(0, 205);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(212, 31);
+            this.panel9.TabIndex = 6;
+            // 
+            // panel10
+            // 
+            this.panel10.Controls.Add(this.BtnKaydet);
+            this.panel10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel10.Location = new System.Drawing.Point(0, 236);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(212, 37);
+            this.panel10.TabIndex = 7;
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnKaydet.Location = new System.Drawing.Point(0, 0);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(212, 37);
+            this.BtnKaydet.TabIndex = 0;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // panel11
+            // 
+            this.panel11.Controls.Add(this.label1);
+            this.panel11.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel11.Location = new System.Drawing.Point(0, 273);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(212, 31);
+            this.panel11.TabIndex = 8;
+            // 
+            // panel12
+            // 
+            this.panel12.Controls.Add(this.TxtMetin);
+            this.panel12.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel12.Location = new System.Drawing.Point(0, 304);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(212, 31);
+            this.panel12.TabIndex = 9;
+            // 
+            // panel13
+            // 
+            this.panel13.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel13.Location = new System.Drawing.Point(0, 335);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(212, 25);
+            this.panel13.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Metni Gir";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel14
+            // 
+            this.panel14.Controls.Add(this.label2);
+            this.panel14.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel14.Location = new System.Drawing.Point(0, 360);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(212, 31);
+            this.panel14.TabIndex = 11;
+            // 
+            // panel15
+            // 
+            this.panel15.Controls.Add(this.TxtBoyut);
+            this.panel15.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel15.Location = new System.Drawing.Point(0, 391);
+            this.panel15.Name = "panel15";
+            this.panel15.Size = new System.Drawing.Size(212, 31);
+            this.panel15.TabIndex = 12;
+            // 
+            // TxtMetin
+            // 
+            this.TxtMetin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TxtMetin.Location = new System.Drawing.Point(0, 0);
+            this.TxtMetin.Name = "TxtMetin";
+            this.TxtMetin.Size = new System.Drawing.Size(212, 26);
+            this.TxtMetin.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(667, 465);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TxtBoyut
+            // 
+            this.TxtBoyut.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TxtBoyut.Location = new System.Drawing.Point(0, 0);
+            this.TxtBoyut.Name = "TxtBoyut";
+            this.TxtBoyut.Size = new System.Drawing.Size(212, 26);
+            this.TxtBoyut.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Boyut";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.BtnKaydet;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 465);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.panel10.ResumeLayout(false);
+            this.panel11.ResumeLayout(false);
+            this.panel12.ResumeLayout(false);
+            this.panel12.PerformLayout();
+            this.panel14.ResumeLayout(false);
+            this.panel15.ResumeLayout(false);
+            this.panel15.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel15;
+        private System.Windows.Forms.TextBox TxtBoyut;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.TextBox TxtMetin;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Button BtnYazdir;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button BtnResimSec;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button BtnRenkSec;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Photo/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Photo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string resim;
+        private void BtnResimSec_Click(object sender, EventArgs e)
+        {
+            // OpenFileDialog fileDialog = new OpenFileDialog();
+            // fileDialog.Filter = "PNG |*.png| Resim Dosyası|*.png";
+            openFileDialog1.Title = "Resim Seç";
+            openFileDialog1.FileName = "Resim Seç";
+            openFileDialog1.Filter = "Jpeg (*.jpeg)|*.jpeg|Jpg (*.jpg)|*.jpg|PNG (*.png)|*.png";
+            openFileDialog1.ShowDialog();
+            resim = openFileDialog1.FileName;
+
+            
+        }
+        Color color;
+        private void BtnRenkSec_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            color = colorDialog1.Color;
+        }
+        Bitmap bitmap;
+        private void BtnYazdir_Click(object sender, EventArgs e)
+        {
+            if (TxtBoyut.Text.Trim() != "" && TxtMetin.Text.Trim() != "")
+            {
+                bitmap = new Bitmap(resim);
+                Graphics gr = Graphics.FromImage(bitmap);
+                gr.DrawString(TxtMetin.Text, new Font("Microsoft Sans Serif", Convert.ToInt16(TxtBoyut.Text), FontStyle.Bold), new SolidBrush(color), 50, 60);
+                pictureBox1.Image = bitmap;
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Girmek İstediğiniz Yazıyı Giriniz ve Yazı Boyutunu Giriniz");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Resim Kaydet";
+            saveFileDialog1.FileName = "Resim Kaydet";
+            saveFileDialog1.Filter = "Jpeg (*.jpeg)|*.jpeg|Jpg (*.jpg)|*.jpg|PNG (*.png)|*.png";
+            saveFileDialog1.ShowDialog();
+            bitmap.Save(saveFileDialog1.FileName);
+        }
+    }
+}
